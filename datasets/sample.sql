051748780cb39e2cac215bc1bb21affd9d1e45dd
Trevor Sinnott <trevor@apexes.co.jp>
2021-05-26 16:58:58 +0900

    Add demo dataset.

---
+++ datasets/sample.sql
@@ -0,0 +1,28 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"ParentId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES(1,'エンタイトルメントの取引先のサンプル','');
+INSERT INTO "Account" VALUES(2,'Demo Account','');
+CREATE TABLE "Delivery_Item__c" (
+	id INTEGER NOT NULL, 
+	"Food_Expiration_Date__c" VARCHAR(255), 
+	"Food_Storage__c" VARCHAR(255), 
+	"Name" VARCHAR(255), 
+	"Delivery__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery_Item__c" VALUES(1,'2021-06-09','Refrigerated','Milk','1');
+INSERT INTO "Delivery_Item__c" VALUES(2,'2021-08-31','Refrigerated','Cheese','1');
+CREATE TABLE "Delivery__c" (
+	id INTEGER NOT NULL, 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	"Supplier__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery__c" VALUES(1,'2021-05-28T03:00:00.000+0000','Requested','2');
+COMMIT;
